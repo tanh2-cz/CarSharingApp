d2746d46438762ef08691f6efb4110ff11cb1367
Sophia-615 <lixinyue1791061878@163.com>
2025-05-06 15:35:12 +0800

    添加群聊详情页面
    2025/5/6

--- frontEnd/CarSharingApp/pages/messages/index.vue
+++ frontEnd/CarSharingApp/pages/messages/index.vue
@@ -3,12 +3,21 @@
     <!-- 顶部搜索栏 -->
     <view class="top-bar">
       <text class="title">拼车</text>
-	  <view class="search-bar">
-	  		<input type="text" v-model="searchQuery" placeholder="搜索消息" class="search-input" />
-	  		<image src="/static/image/icon_search.png" class="search-icon"></image>
-	  </view>
+      <view class="search-bar">
+        <input
+          type="text"
+          v-model="searchQuery"
+          placeholder="搜索消息"
+          class="search-input"
+        />
+        <image src="/static/image/icon_search.png" class="search-icon"></image>
+      </view>
       <view class="filter-buttons" @tap="toggleFilter">
-        <image src="/static/image/icon_filter.png" class="filter-icon" @tap="showFilter"></image>
+        <image
+          src="/static/image/icon_filter.png"
+          class="filter-icon"
+          @tap="showFilter"
+        ></image>
       </view>
     </view>
 
@@ -17,15 +26,29 @@
       <view class="filter-content">
         <view class="filter-header">
           <text>高级筛选</text>
-          <image src="/static/icon_close.png" class="close-icon" @tap="toggleFilter"></image>
+          <image
+            src="/static/icon_close.png"
+            class="close-icon"
+            @tap="toggleFilter"
+          ></image>
         </view>
         <view class="filter-item">
           <text>出发地</text>
-          <input type="text" v-model="filter.startLocation" placeholder="请输入出发地" class="filter-input" />
+          <input
+            type="text"
+            v-model="filter.startLocation"
+            placeholder="请输入出发地"
+            class="filter-input"
+          />
         </view>
         <view class="filter-item">
           <text>目的地</text>
-          <input type="text" v-model="filter.endLocation" placeholder="请输入目的地" class="filter-input" />
+          <input
+            type="text"
+            v-model="filter.endLocation"
+            placeholder="请输入目的地"
+            class="filter-input"
+          />
         </view>
         <view class="filter-item">
           <text>出发时间</text>
@@ -33,14 +56,18 @@
             <view class="picker">{{ filter.time || "请选择时间" }}</view>
           </picker>
         </view>
-		<!-- 价位 -->
+        <!-- 价位 -->
         <button class="apply-btn" @tap="applyFilter">应用筛选</button>
       </view>
     </view>
 
     <!-- 拼车通知列表 -->
     <scroll-view scroll-y class="ride-list">
-      <view class="ride-item" v-for="(ride, index) in filteredRides" :key="index" @tap="viewRideDetail(ride)">
+      <view
+        class="ride-item"
+        v-for="(ride, index) in filteredRides"
+        :key="index"
+      >
         <view class="ride-info">
           <view class="route">
             <text class="start">{{ ride.startLocation }}</text>
@@ -52,20 +79,23 @@
             <text class="text-price">价格: ¥{{ ride.price }}</text>
           </view>
         </view>
-		<!-- 司机视角 待接单 已接单 -->
-<!--        <view class="ride-status">
+        <!-- 司机视角 待接单 已接单 -->
+        <!--        <view class="ride-status">
           <text>{{ ride.status }}</text>
         </view> -->
-		<!-- 用户视角 是否已加入群聊 群聊按钮 -->
-		<view class="ride-status">
-		  <button v-if="ride.status === 0" class="status-button disabled" disabled>
-		    已加群
-		  </button>
-		  <button v-else class="status-button" @click="goToGroupDetails">
-		    加群去拼车
-		  </button>
-		</view>
-		
+        <!-- 用户视角 是否已加入群聊 群聊按钮 -->
+        <view class="ride-status">
+          <button
+            v-if="ride.status === 0"
+            class="status-button disabled"
+            disabled
+          >
+            已加群
+          </button>
+          <button v-else class="status-button" @click="goToGroupDetails(ride)">
+            加群去拼车
+          </button>
+        </view>
       </view>
       <view class="no-data" v-if="!filteredRides.length">暂无拼车消息</view>
     </scroll-view>
@@ -76,24 +106,80 @@
 export default {
   data() {
     return {
-      searchQuery: '',
+      searchQuery: "",
       showFilter: false,
       filter: {
-        startLocation: '',
-        endLocation: '',
-        time: ''
+        startLocation: "",
+        endLocation: "",
+        time: "",
       },
       rides: [
-        { id: 1, startLocation: '上海大学', endLocation: '虹桥机场', time: '14:30', price: 50, status: '待接单' },
-        { id: 2, startLocation: '浦东机场', endLocation: '人民广场', time: '16:00', price: 80, status: '已接单' },
-        { id: 3, startLocation: '徐家汇', endLocation: '静安寺', time: '09:00', price: 30, status: '待接单' },
-		{ id: 4, startLocation: '同济大学', endLocation: '五角场', time: '09:00', price: 30, status: '待接单' },
-		{ id: 5, startLocation: '五角场', endLocation: '上海交通大学', time: '09:00', price: 30, status: '待接单' },
-		{ id: 6, startLocation: '嘉定', endLocation: '豫园', time: '09:00', price: 30, status: '待接单' },
-		{ id: 7, startLocation: '徐家汇', endLocation: '嘉定', time: '09:00', price: 30, status: '待接单' },
-		{ id: 0, startLocation: '徐家汇', endLocation: '嘉定', time: '09:00', price: 30, status: '待接单' },
+        {
+          id: 1,
+          startLocation: "上海大学",
+          endLocation: "虹桥机场",
+          time: "14:30",
+          price: 50,
+          status: "待接单",
+        },
+        {
+          id: 2,
+          startLocation: "浦东机场",
+          endLocation: "人民广场",
+          time: "16:00",
+          price: 80,
+          status: "已接单",
+        },
+        {
+          id: 3,
+          startLocation: "徐家汇",
+          endLocation: "静安寺",
+          time: "09:00",
+          price: 30,
+          status: "待接单",
+        },
+        {
+          id: 4,
+          startLocation: "同济大学",
+          endLocation: "五角场",
+          time: "09:00",
+          price: 30,
+          status: "待接单",
+        },
+        {
+          id: 5,
+          startLocation: "五角场",
+          endLocation: "上海交通大学",
+          time: "09:00",
+          price: 30,
+          status: "待接单",
+        },
+        {
+          id: 6,
+          startLocation: "嘉定",
+          endLocation: "豫园",
+          time: "09:00",
+          price: 30,
+          status: "待接单",
+        },
+        {
+          id: 7,
+          startLocation: "徐家汇",
+          endLocation: "嘉定",
+          time: "09:00",
+          price: 30,
+          status: "待接单",
+        },
+        {
+          id: 0,
+          startLocation: "徐家汇",
+          endLocation: "嘉定",
+          time: "09:00",
+          price: 30,
+          status: "待接单",
+        },
       ],
-      filteredRides: []
+      filteredRides: [],
     };
   },
   onLoad() {
@@ -106,36 +192,51 @@ export default {
     updateTime(e) {
       this.filter.time = e.detail.value;
     },
-	goToGroupDetails() {
-	    // 假设详情页面的路由路径为 /details
-		uni.showToast({
-		  title: '查看群聊详细信息待实现',
-		  icon: 'none'
-		});
-		},
-	    // uni.navigateTo({
-	    //   url: '/pages/details/details'
-	    // });
+    goToGroupDetails() {
+      // 假设详情页面的路由路径为 /details
+      // uni.showToast({
+      //   title: '查看群聊详细信息待实现',
+      //   icon: 'none'
+      // });
+      // },
+      uni.navigateTo({
+        url: "/pages/groupDetails/index?id=${rides.id}",
+      });
+    },
     applyFilter() {
       // 筛选逻辑
-      this.filteredRides = this.rides.filter(ride => {
-        const matchStart = this.filter.startLocation ? ride.startLocation.includes(this.filter.startLocation) : true;
-        const matchEnd = this.filter.endLocation ? ride.endLocation.includes(this.filter.endLocation) : true;
-        const matchTime = this.filter.time ? ride.time === this.filter.time : true;
-        const matchQuery = this.searchQuery ? (ride.startLocation.includes(this.searchQuery) || ride.endLocation.includes(this.searchQuery)) : true;
+      this.filteredRides = this.rides.filter((ride) => {
+        const matchStart = this.filter.startLocation
+          ? ride.startLocation.includes(this.filter.startLocation)
+          : true;
+        const matchEnd = this.filter.endLocation
+          ? ride.endLocation.includes(this.filter.endLocation)
+          : true;
+        const matchTime = this.filter.time
+          ? ride.time === this.filter.time
+          : true;
+        const matchQuery = this.searchQuery
+          ? ride.startLocation.includes(this.searchQuery) ||
+            ride.endLocation.includes(this.searchQuery)
+          : true;
         return matchStart && matchEnd && matchTime && matchQuery;
       });
       this.showFilter = false;
     },
     viewRideDetail(ride) {
-		//预计下面弹出来一个简略的信息，点击其他地方自动收回去，然后点击加群可以显示详细信息，专门一个页面显示
       uni.showToast({
         title: `查看拼车单简略信息待实现: ${ride.id}`,
-        icon: 'none'
+        icon: "none",
       });
-    }
-  }
-}
+    },
+    updateRideStatus(rideId, status) {
+      const index = this.filteredRides.findIndex((ride) => ride.id === rideId);
+      if (index !== -1) {
+        this.filteredRides[index].status = status.isJoined ? 0 : 1;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -192,7 +293,7 @@ export default {
   font-size: 30rpx;
   color: #333;
   /* background-color: #28a745; */
-  margin-left:7%;
+  margin-left: 7%;
 }
 
 .filter-btn {
@@ -252,7 +353,8 @@ export default {
   margin-bottom: 10rpx;
 }
 
-.filter-input, .picker {
+.filter-input,
+.picker {
   background: #f5f5f5;
   border-radius: 12rpx;
   padding: 20rpx;
@@ -272,7 +374,8 @@ export default {
 
 .ride-list {
   flex: 1;
-  background: #fff;  flex: 1;
+  background: #fff;
+  flex: 1;
   background: #fff;
   padding: 20rpx 0;
   margin-top: 12%;
@@ -292,7 +395,7 @@ export default {
   margin-bottom: 20rpx; /* 卡片之间的间距 */
   transition: transform 0.3s ease, box-shadow 0.3s ease; /* 过渡效果 */
   position: relative; /* 相对定位 */
-  margin:2%;
+  margin: 2%;
   width: 88%;
 }
 
@@ -323,25 +426,26 @@ export default {
 
 .text-time {
   font-size: 30rpx;
-  color: #007BFF; /* 蓝色提示颜色 */
-  font-family: 'SimHei', sans-serif; /* 调整字体类型 */
+  color: #007bff; /* 蓝色提示颜色 */
+  font-family: "SimHei", sans-serif; /* 调整字体类型 */
   font-weight: bold; /* 加粗 */
   margin-left: 2%;
 }
 
 .text-price {
   font-size: 30rpx;
-  color: #28A745; /* 绿色提示颜色 */
+  color: #28a745; /* 绿色提示颜色 */
   margin-left: 10%; /* 左边距 */
-  font-family:  'SimHei', sans-serif; /* 调整字体类型 */
+  font-family: "SimHei", sans-serif; /* 调整字体类型 */
   font-weight: bold; /* 加粗 */
 }
 
-.start, .end {
+.start,
+.end {
   font-size: 36rpx; /* 字体大小 */
   color: #333; /* 字体颜色 */
   font-weight: bold; /* 加粗 */
-  font-family: 'PingFang SC', 'Microsoft YaHei', 'SimHei', sans-serif; /* 设置中文字体 */
+  font-family: "PingFang SC", "Microsoft YaHei", "SimHei", sans-serif; /* 设置中文字体 */
   overflow: hidden; /* 隐藏超出部分 */
   text-overflow: ellipsis; /* 显示省略号 */
   white-space: nowrap; /* 不换行 */
@@ -351,7 +455,6 @@ export default {
   margin: 0 10rpx;
   color: #000;
 }
-
 
 .no-data {
   padding: 40rpx;
@@ -381,17 +484,17 @@ export default {
   opacity: 0.8;
 }
 
-.ride-status{
-	margin-right: 2%;
+.ride-status {
+  margin-right: 2%;
 }
 
 .status-button {
-  width:120%;
-  height:40%;
+  width: 120%;
+  height: 40%;
   border-radius: 4rpx;
   font-size: 26rpx;
   color: #fff;
-  font-family: 'PingFang SC', 'Microsoft YaHei', 'SimHei', sans-serif; /* 设置中文字体 */
+  font-family: "PingFang SC", "Microsoft YaHei", "SimHei", sans-serif; /* 设置中文字体 */
   background-color: #ffaa00;
   font-weight: bold;
   transition: background-color 0.3s ease;
